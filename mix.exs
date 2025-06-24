03c4447946850a5a6c9eaea4f16be040a39eef06
Eduardo Cunha <eduardo.cunha@ist.utl.pt>
2025-06-24 11:33:31 +0100

    chore: release v0.1.3
    Signed-off-by: Eduardo Cunha <eduardo.cunha@ist.utl.pt>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpenFeature.MixProject do
   def project do
     [
       app: :open_feature,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       test_paths: ["test/unit", "test/integration"],
